dc896dbaf9c5de9494fd5eb4999688b92bd7b72b
Thomas Griesmayer <griesmayer@spengergasse.at>
2020-03-27 20:50:16 +0100

    Exception class

---
+++ src/domain/BankAccountException.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class BankAccountException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+
+	public BankAccountException(String message)
+	{
+		super(message);
+	}
+}
